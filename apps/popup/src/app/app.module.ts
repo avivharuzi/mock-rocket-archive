1df06b00a997194d637d05785ed232281609471a
Aviv Haruzi <avivharuzi@gmail.com>
2022-05-27 08:33:19 +0300

    feat: 🎸 add angular material

--- apps/popup/src/app/app.module.ts
+++ apps/popup/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CodemirrorModule } from './ui/codemirror/codemirror.module';
 
 @NgModule({
-  imports: [BrowserModule, CodemirrorModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    CodemirrorModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
