16cf295e3b5522793c885f7aa416f9ad87c36343
Aviv Haruzi <avivharuzi@gmail.com>
2022-05-27 11:56:26 +0300

    feat: 🎸 add http methods

---
+++ apps/popup/src/app/http/http-method.ts
@@ -0,0 +1,19 @@
+enum HTTPMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
+  COPY = 'COPY',
+  HEAD = 'HEAD',
+  OPTIONS = 'OPTIONS',
+  LINK = 'LINK',
+  UNLINK = 'UNLINK',
+  PURGE = 'PURGE',
+  LOCK = 'LOCK',
+  UNLOCK = 'UNLOCK',
+  PROPFIND = 'PROPFIND',
+  VIEW = 'VIEW',
+}
+
+export const HTTP_METHODS = Object.values(HTTPMethod);
