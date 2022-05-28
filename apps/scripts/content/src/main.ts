d6a165b0b8a5af2587d07e26ac22041a65f67a75
Aviv Haruzi <avivharuzi@gmail.com>
2022-05-28 13:06:17 +0300

    feat: 🎸 add inject script

--- apps/scripts/content/src/main.ts
+++ apps/scripts/content/src/main.ts
@@ -3,3 +3,9 @@ chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
     sendResponse({ host: location.host });
   }
 });
+
+(() => {
+  const script = document.createElement('script');
+  script.src = chrome.runtime.getURL('scripts/inject/main.esm.js');
+  (document.head || document.documentElement).appendChild(script);
+})();
